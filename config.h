6a9e78fe88c3aa64d221d6f75b8bc39e585af0f5
Faisal Richard D. Tamano Jr <84402344+FaisalTamanoJr@users.noreply.github.com>
2021-12-01 08:08:33 +0300

    Update config.h

--- config.h
+++ config.h
@@ -16,10 +16,10 @@ static int smartgaps          = 0;        /* 1 means no outer gap when there is 
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "fira code:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
-static char normbgcolor[]           = "#1d2021";
-static char normbordercolor[]       = "#1d2021";
+static char normbgcolor[]           = "#282828";
+static char normbordercolor[]       = "#282828";
 static char normfgcolor[]           = "#458588";
-static char selfgcolor[]            = "#1d2021";
+static char selfgcolor[]            = "#282828";
 static char selbordercolor[]        = "#928374";
 static char selbgcolor[]            = "#458588";
 static char *colors[][3] = {
